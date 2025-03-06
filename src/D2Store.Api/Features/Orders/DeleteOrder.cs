5320cf9f6a57743c6faed640c15de432711b9285
Artur Pietrzyk <arturpietrzyk96@gmail.com>
2025-03-06 17:05:51 +0000

    add uppdate and delete endpopints, introduce dtos

---
+++ src/D2Store.Api/Features/Orders/DeleteOrder.cs
@@ -0,0 +1,31 @@
+﻿using D2Store.Api.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace D2Store.Api.Features.Orders;
+
+public record DeleteOrderCommand(Guid OrderId) : IRequest<Guid>; 
+
+public class DeleteOrderHander : IRequestHandler<DeleteOrderCommand, Guid> 
+{
+    private readonly AppDbContext _dbContext;
+
+    public DeleteOrderHander(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Guid> Handle(DeleteOrderCommand request, CancellationToken cancellationToken) 
+    {
+        var order = await _dbContext.Orders
+           .FirstOrDefaultAsync(o => o.Id == request.OrderId, cancellationToken);
+        //if (order == null)
+        //{
+        //    throw new KeyNotFoundException($"Order with ID {request.OrderId} not found.");
+        //}
+        _dbContext.Orders.Remove(order);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return order.Id;
+    }
+}
+
